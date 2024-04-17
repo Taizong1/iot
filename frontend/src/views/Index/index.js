a2179dcda971a1b63aa4143b2c619771fe572047
zappliel <cp15251276582@outlook.com>
2024-04-17 12:07:38 +0800

    reference

--- frontend/src/views/Index/index.js
+++ frontend/src/views/Index/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Breadcrumb, Layout, Menu, theme } from 'antd';
-
+import bgImage from '../../assets/images/home.png'
 const Index = () => {  
   return (  
-    <div>  
+    <div>
       <Breadcrumb style={{ margin: '16px 0' }}>
           <Breadcrumb.Item>Home</Breadcrumb.Item>
           <Breadcrumb.Item>List</Breadcrumb.Item>
@@ -11,13 +11,13 @@ const Index = () => {
         </Breadcrumb>
         <div
           style={{  
-            background: 'white', // Set the background color to white  
-            minHeight: 280,  
-            padding: 24,  
-            border: '1px solid #e8e8e8', // Add a 1px solid border with the specified color  
+            backgroundImage: `url(${bgImage})`, // Set the background color to white
+            minHeight: 600,
+            padding: 24,
+            border: '1px solid #e8e8e8', // Add a 1px solid border with the specified color
           }}  
         >
-          Content
+
         </div>
     </div>  
   );  
