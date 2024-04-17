5d344d5eb49b5f33bbebffa1135f62fcbb065c1a
Taizong1 <1252219871@qq.com>
2024-04-17 21:50:37 +0800

    Model: update phoneNo and emailAdress

--- src/components/reducer/action.js
+++ src/components/reducer/action.js
@@ -1,6 +1,7 @@
 // 定义action类型
 export const USER_LOGIN = 'USER_LOGIN';
 export const USER_UNLOGIN = 'USER_UNLOGIN';
+export const UPDATE_USER_INFO = 'UPDATE_USER_INFO';
 
 
 // 定义action creators
@@ -14,4 +15,12 @@ export const userLogin = (userName, userId) => ({
 
 export const userUnLogin = () => ({
   type: USER_UNLOGIN
+});
+
+export const updateUserInfo = () => ({
+  type: UPDATE_USER_INFO,
+  payload: {
+    emailAddress,
+    phoneNo
+  }
 });
--- src/components/reducer/reducer.js
+++ src/components/reducer/reducer.js
@@ -5,11 +5,15 @@ import { USER_LOGIN, USER_UNLOGIN } from './action';
 isLogin 是否登录
 userId 用户Id
 userName 用户名
+emailAddress 邮箱
+phoneNo 电话号码
 */
 const initialState = {
     isLogin: false,
     userId: 0,
     userName: "",
+    emailAdress: "",
+    phoneNo: "",
 };
 
 // 定义reducer函数
@@ -21,7 +25,9 @@ const IotReducer = (state = initialState, action) => {
             ...state,
             isLogin: true,
             userId: action.payload.userId,
-            userName: action.payload.userName
+            userName: action.payload.userName,
+            emailAdress: action.payload.emailAdress,
+            phoneNo: action.payload.phoneNo
         }
     }
     case USER_UNLOGIN: {
@@ -30,7 +36,17 @@ const IotReducer = (state = initialState, action) => {
             ...state,
             isLogin: false,
             userId: 0,
-            userName: ""
+            userName: "",
+            emailAdress: "",
+            phoneNo: ""
+        }
+    }
+    case UPDATE_USER_INFO: {
+        console.log("update user information")
+        return {
+            ...state,
+            emailAdress: action.payload.emailAdress,
+            phoneNo: action.payload.phoneNo
         }
     }
     default:
